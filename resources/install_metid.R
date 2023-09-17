96bc3e4957058d2952427261250ed9097ebeea9d
Paulo Cilas Morais Lyra Junior <pj_ibm@hotmail.com>
2023-09-17 16:37:26 -0400

    paulo changes

--- resources/install_metid.R
+++
@@ -1,2 +0,0 @@
-#calling from terminal doesn't work so trying script
-remotes::install_github("tidymass/metid", dependencies = TRUE)
--- resources/install_stringi.R
+++
@@ -1,3 +0,0 @@
-#flexible install of stringi
-f = list.files(".", pattern = ".tar.gz")
-install.packages(f, repos = NULL, type = "source")
